bbf9764579ed780d49f635ebc3cde0b54dd58223
niraj bava <nirajbava222@gmail.com>
2022-12-28 00:00:36 +0530

    add new code

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let dead = document.getElementById("dead");
 
 let recovered = document.getElementById("recovered");
 
-let active =  document.getElementById("active");
+let active = document.getElementById("active");
 
 let a;
 
@@ -19,70 +19,86 @@ let a;
 
 
 
-function getData(url){
+function getData(url) {
 
-  
-    fetch(url).then((response)=>{
-        return response.json();
-    }).then((data)=>{
+
+  fetch(url).then((response) => {
+    return response.json();
+  }).then((data) => {
+
+console.log(data);
+    
+    let av = 0 , v=0, d=0, r=0;
+
+    for (var i = 0; i < data.length; i++) {
+
       
-      console.log(data)
-      
-      console.log(data[0].new_active)
-      
-      
-      for (var i = 1; i<data.length; i++) {
+      v += parseInt(data[i].new_positive);
+      av += parseInt(data[i].new_active);
+      d += parseInt(data[i].new_death);
+      r += parseInt(data[i].new_cured);
+    
+
+      var node = document.createElement('option');
+
+
+      node.setAttribute("value", i);
+
+     
+
         
-        var node = document.createElement('option');
-           
-           
-        node.setAttribute("value", i);
-        
-       
-        
-        var textnode = document.createTextNode(data[i].state_name);
-      
-       node.appendChild(textnode);
-      
+      var textnode = document.createTextNode(data[i].state_name);
+
+      node.appendChild(textnode);
+
       document.getElementById("statelist").appendChild(node);
-      
-      }
-        
-      country.innerText = data[0].length;
 
-      victims.innerText = data[0].new_active;
-        
-      dead.innerText = data[0];
-      
-      recovered.innerText = data[0].recovered;
-        
-        
-      a = data;
-      
-    })
+
+
+    }
+
+
+    country.innerText = data.length;
+
+    victims.innerText = v;
+
+    active.innerText = av;
+  
+    dead.innerText = d;
+  
+    recovered.innerText = r;
+ 
+  
+
+    a = data;
+
+
+  })
 }
 
-function getstate(){
-      
-    statevalue = document.getElementById("statelist").value ;
-      
-      
-      country.innerText = a[statevalue].state_name;
-      
-      victims.innerHTML = a[statevalue].new_positive;
 
-      active.innerHTML = a[statevalue].new_active;
-      
-      dead.innerText = a[statevalue].new_death;
-      
-      
-    recovered.innerText = a[statevalue].new_cured;
-      
-      
-      
-      console.log(a[statevalue]);
-      
-      
+
+function getstate() {
+
+  statevalue = document.getElementById("statelist").value;
+
+
+  country.innerText = a[statevalue].state_name;
+
+  victims.innerHTML = a[statevalue].new_positive;
+
+  active.innerHTML = a[statevalue].new_active;
+
+  dead.innerText = a[statevalue].new_death;
+
+
+  recovered.innerText = a[statevalue].new_cured;
+
+
+
+  console.log(a[statevalue]);
+
+
 }
 const proxy = "https://www.mohfw.gov.in/data/datanew.json";
 
